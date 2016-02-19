2a4e38b1006f022937a0012a7f8f9ca75c72e957
Michael Missouri <michaelmissouri@gmail.com>
2016-02-19 16:15:06 -0500

    Final project for Udacity BiOS committed

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
+        self.nameField.autocapitalizationType = .Words
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(sender: AnyObject) {
+        nameField.text = ""
+        lyricsView.text = ""
+        nameField.text?.capitalizedString
+    }
+    
+    @IBAction func displayLyrics(sender: AnyObject) {
+        if nameField.text != "" {
+            lyricsView.text = lyricsForName(bananaFanaTemplate, fullName: nameField.text!)
+        }
+    }
 
 }
+
+func shortNameFromName(name: String) -> String {
+    
+    let lowercaseName = name.lowercaseString
+    
+    let vowelSet = NSCharacterSet(charactersInString: "aeiou")
+ 
+    if let firstVowelRange = lowercaseName.rangeOfCharacterFromSet(vowelSet, options: .CaseInsensitiveSearch) {
+        return lowercaseName.substringFromIndex(firstVowelRange.startIndex)
+    }
+
+    return lowercaseName
+}
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameFromName(fullName)
+    
+    let lyrics = lyricsTemplate
+        .stringByReplacingOccurrencesOfString("<FULL_NAME>", withString: fullName)
+        .stringByReplacingOccurrencesOfString("<SHORT_NAME>", withString: shortName)
+    
+    return lyrics
+}
+
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joinWithSeparator("\n")
+
+
 
